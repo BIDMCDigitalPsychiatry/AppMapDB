19c0c63741b29816444c4d935499de54068cd2d1
cvanem <chris@greenlinkservices.com>
2020-04-03 21:55:56 +0000

    Deploying to gh-pages from  @ 0049465090f6e0058c3f9840e5ffbf9395ef1b80 🚀

---
+++ precache-manifest.5a99de51bff6bcc08c7f6a3539ec7ff7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346d2375d17fbeba9e12d9378a6061aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c76465777b8c1ccb9b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a6a21550416036150fa4",
+    "url": "/static/js/2.53a359e4.chunk.js"
+  },
+  {
+    "revision": "cd5da68083c98404b032f6baa59ea0fb",
+    "url": "/static/js/2.53a359e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedfac848649e9232f7f",
+    "url": "/static/js/3.4d581e75.chunk.js"
+  },
+  {
+    "revision": "30ae972adbd13888434c",
+    "url": "/static/js/4.eeb40976.chunk.js"
+  },
+  {
+    "revision": "b5c76465777b8c1ccb9b",
+    "url": "/static/js/main.4fdfcda4.chunk.js"
+  },
+  {
+    "revision": "28063ebea3ecc42afe47",
+    "url": "/static/js/runtime-main.a48e9bc3.js"
+  },
+  {
+    "revision": "0ae09ead31de9c341e026098163645e7",
+    "url": "/static/media/app_model.0ae09ead.png"
+  },
+  {
+    "revision": "f4fb2b9c4d69576b5c0f8b559b0879aa",
+    "url": "/static/media/apple_store.f4fb2b9c.png"
+  },
+  {
+    "revision": "0eb0fba9639d41fb048832b89d926c44",
+    "url": "/static/media/default_app_icon.0eb0fba9.png"
+  },
+  {
+    "revision": "decc32d0f8bc952092eca7e26286fb29",
+    "url": "/static/media/google_play_store.decc32d0.png"
+  },
+  {
+    "revision": "742ee37cc969f81409d0ab2835868a57",
+    "url": "/static/media/logo.742ee37c.png"
+  },
+  {
+    "revision": "4fd682c0d5819c1eeb06272b7f2968cc",
+    "url": "/static/media/rateappscreenshot.4fd682c0.png"
+  },
+  {
+    "revision": "f3e8e2755d730a42a65fd0a620075ce8",
+    "url": "/static/media/web.f3e8e275.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b93a500c4b842e819a30b99731914e5.js"
+  "/precache-manifest.5a99de51bff6bcc08c7f6a3539ec7ff7.js"
 );
 
 self.addEventListener('message', (event) => {
