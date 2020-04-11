0556fc289c6c5f1ad18560a1f926f6d82df9d2ca
Chris V <chris@greenlinkservices.com>
2020-04-11 11:54:18 -0600

    [AdminToggle] Hide for non admin users

--- src/components/application/GenericTable/Applications/AdminToggle.tsx
+++ src/components/application/GenericTable/Applications/AdminToggle.tsx
@@ -2,13 +2,14 @@
 import * as Icons from '@material-ui/icons';
 import { useAdminMode } from '../../../layout/store';
 import DialogButton from '../../GenericDialog/DialogButton';
-import { useFullScreen } from '../../../../hooks';
+import { useFullScreen, useIsAdmin } from '../../../../hooks';
 
 export default function AdminToggle() {
+  const isAdmin = useIsAdmin();
   const [adminMode, setAdminMode] = useAdminMode() as any;
   const handleClick = React.useCallback(() => setAdminMode(adminMode === true ? false : true), [adminMode, setAdminMode]);
   const fullScreen = useFullScreen();
-  return (
+  return isAdmin ? (
     <DialogButton
       Icon={adminMode === true ? Icons.PermIdentity : Icons.PeopleAlt}
       tooltip={`Switch to ${adminMode === true ? 'public mode' : 'admin mode'}`}
@@ -16,5 +17,7 @@ export default function AdminToggle() {
     >
       {!fullScreen && <div style={{ width: fullScreen ? undefined : 130 }}> {adminMode === true ? 'Viewing Admin' : 'Viewing Public'}</div>}
     </DialogButton>
+  ) : (
+    <></>
   );
 }
