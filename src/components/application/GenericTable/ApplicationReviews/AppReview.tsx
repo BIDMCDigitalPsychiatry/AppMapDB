e507542fe8a47f43136b00ce4fcbeec17c922680
Chris Van Emmerik <chris@greenlinkservices.com>
2020-06-13 09:35:20 -0600

    [AppReview] Expand/collapse qualitative review text

--- src/components/application/GenericTable/ApplicationReviews/AppReview.tsx
+++ src/components/application/GenericTable/ApplicationReviews/AppReview.tsx
@@ -1,36 +1,63 @@
 ﻿import React from 'react';
 import { Grid, Box, Typography } from '@material-ui/core';
-import Application from '../../../../database/models/Application';
 import OutlinedDiv from '../../../general/OutlinedDiv/OutlinedDiv';
 import { getDayTimeFromTimestamp } from '../../../../helpers';
+import { makeStyles, createStyles } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(({ palette }: any) =>
+  createStyles({
+    container: {
+      cursor: 'pointer',
+      '&:hover': {
+        background: grey[100]
+      }
+    }
+  })
+);
 
 const appColumnWidth = 520;
 
-export default function AppReview({ updated, review }: Application) {
+export default function AppReview({ updated, review, handleRefresh }) {
+  const [expand, setExpand] = React.useState(false);
+  const handleExpand = React.useCallback(
+    e => {
+      setExpand(prev => !prev);
+      handleRefresh();
+    },
+    [setExpand, handleRefresh]
+  );
+
+  const classes = useStyles();
   return (
-    <OutlinedDiv>
-      <Box pt={1} pb={1}>
-        <Grid container spacing={2}>
-          <Grid item style={{ width: appColumnWidth }}>
-            <Grid container spacing={2}>
-              <Grid item zeroMinWidth xs>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Typography noWrap variant='h6'>
-                      {review}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography noWrap color='textSecondary' variant='caption'>
-                      Last Updated: {updated ? getDayTimeFromTimestamp(updated) : ''}
-                    </Typography>
+    <div className={classes.container} onClick={handleExpand}>
+      <OutlinedDiv>
+        <Box pt={1} pb={1}>
+          <Grid container>
+            <Grid item style={{ width: appColumnWidth }}>
+              <Grid container>
+                <Grid item zeroMinWidth xs>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <Typography noWrap={!expand}>{review}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Grid container justify='space-between'>
+                        <Typography noWrap color='textSecondary' variant='caption'>
+                          Last Updated: {updated ? getDayTimeFromTimestamp(updated) : ''}
+                        </Typography>
+                        <Typography noWrap color='textSecondary' variant='caption'>
+                          Click to {expand ? 'collapse' : 'expand'}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Box>
-    </OutlinedDiv>
+        </Box>
+      </OutlinedDiv>
+    </div>
   );
 }
