fa913f336543486760bcdb77dbf30840e7f1dc31
cvanem <chris@greenlinkservices.com>
2020-07-15 00:01:33 +0000

    Deploying to gh-pages from  @ 7c1213c66a0c0ec2a3affd6be4f66a82feb90044 🚀

---
+++ precache-manifest.36a9bef25bb4ea9df97e8f73d312dc02.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf1c94123f53d7a872674515b5b9fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fad6b2e736eb1d6e7fd6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "34ba4856c8acf647ebf0",
+    "url": "/static/js/2.6ded5a14.chunk.js"
+  },
+  {
+    "revision": "b340be4b0ca8313d0e7c2946d27d56a1",
+    "url": "/static/js/2.6ded5a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09166087203b702966d",
+    "url": "/static/js/3.d21e3f63.chunk.js"
+  },
+  {
+    "revision": "228e6db95bfc8dab2af8",
+    "url": "/static/js/4.195922fe.chunk.js"
+  },
+  {
+    "revision": "fad6b2e736eb1d6e7fd6",
+    "url": "/static/js/main.d7c0891a.chunk.js"
+  },
+  {
+    "revision": "123f94b49bd2a547dbcc",
+    "url": "/static/js/runtime-main.0a525148.js"
+  },
+  {
+    "revision": "228fbf0b8733bb4bb36b5f449f04f8de",
+    "url": "/static/media/0.228fbf0b.png"
+  },
+  {
+    "revision": "f29b2154d6bdd3202bf5cb2890ede80f",
+    "url": "/static/media/1.f29b2154.png"
+  },
+  {
+    "revision": "5f579cef5fc2c90d7d6d368b0e83cf04",
+    "url": "/static/media/10.5f579cef.png"
+  },
+  {
+    "revision": "e103d6bfb6c807a9d331796557f592ed",
+    "url": "/static/media/11.e103d6bf.png"
+  },
+  {
+    "revision": "c061fcdf8b03abf3f3ee7def5f63f6a2",
+    "url": "/static/media/2.c061fcdf.png"
+  },
+  {
+    "revision": "4e617c1519dbfa8daf5236de481953de",
+    "url": "/static/media/3.4e617c15.png"
+  },
+  {
+    "revision": "c3619205ca3e0f8a2c3eda6f97f1fe6e",
+    "url": "/static/media/4.c3619205.png"
+  },
+  {
+    "revision": "38f6e83db56a15ba0429007bd27b4f1a",
+    "url": "/static/media/5.38f6e83d.png"
+  },
+  {
+    "revision": "7f8298f89cf6208e95a40931bbcbd850",
+    "url": "/static/media/6.7f8298f8.png"
+  },
+  {
+    "revision": "fa83619a0814dc6fb10bcbf3d042acfd",
+    "url": "/static/media/7.fa83619a.png"
+  },
+  {
+    "revision": "390ea31d68dd9758d7181fa52a281435",
+    "url": "/static/media/8.390ea31d.png"
+  },
+  {
+    "revision": "72e1e30b5cf49b7a5d9d02af2956dbd5",
+    "url": "/static/media/9.72e1e30b.png"
+  },
+  {
+    "revision": "a2d6e854d5a0812de29181c8ed33f44a",
+    "url": "/static/media/FrameworkQuestions.a2d6e854.md"
+  },
+  {
+    "revision": "c788d48070a0ee8a14ca2ae861c67809",
+    "url": "/static/media/News.c788d480.md"
+  },
+  {
+    "revision": "2ee3ceda2c76510720465049b5500198",
+    "url": "/static/media/ObjectiveQuestions.2ee3ceda.md"
+  },
+  {
+    "revision": "e327de2094a9dd468f5ff67ce6963313",
+    "url": "/static/media/app_model.e327de20.png"
+  },
+  {
+    "revision": "f4fb2b9c4d69576b5c0f8b559b0879aa",
+    "url": "/static/media/apple_store.f4fb2b9c.png"
+  },
+  {
+    "revision": "0eb0fba9639d41fb048832b89d926c44",
+    "url": "/static/media/default_app_icon.0eb0fba9.png"
+  },
+  {
+    "revision": "decc32d0f8bc952092eca7e26286fb29",
+    "url": "/static/media/google_play_store.decc32d0.png"
+  },
+  {
+    "revision": "742ee37cc969f81409d0ab2835868a57",
+    "url": "/static/media/logo.742ee37c.png"
+  },
+  {
+    "revision": "f3e8e2755d730a42a65fd0a620075ce8",
+    "url": "/static/media/web.f3e8e275.png"
+  },
+  {
+    "revision": "7510e6ebde11c895462672533113b65d",
+    "url": "/static/media/zoom_0.7510e6eb.mp4"
+  },
+  {
+    "revision": "9e66c7aac26e00fb70fbdc9df19f47f0",
+    "url": "/static/media/zoom_1.9e66c7aa.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c06ba27a8b8c7b3beb9402558dc44839.js"
+  "/precache-manifest.36a9bef25bb4ea9df97e8f73d312dc02.js"
 );
 
 self.addEventListener('message', (event) => {
