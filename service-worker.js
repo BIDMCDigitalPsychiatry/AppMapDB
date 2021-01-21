1c0c298a485382d13a0c22f550d926a4865a6ee0
cvanem <chris@greenlinkservices.com>
2021-01-21 20:23:23 +0000

    Deploying to gh-pages from  @ dae5b775572c0a45e119bbcf9d882031da35d870 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abc2733317a6f18245ee0df05f7cf9c5.js"
+  "/precache-manifest.b6b62c3995f49ca98a6a49d81cd9cf83.js"
 );
 
 self.addEventListener('message', (event) => {
