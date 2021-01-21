e679dbc6370f5eea20780139ebf09dc7a8bc5fd7
Chris Van Emmerik <chris@greenlinkservices.com>
2021-01-21 15:15:07 -0700

    [ViewApp] Adjust header layout and add info fields

--- src/components/pages/ViewApp.tsx
+++ src/components/pages/ViewApp.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Typography, createStyles, makeStyles } from '@material-ui/core';
+import { Grid, Typography, createStyles, makeStyles, Divider } from '@material-ui/core';
 import { useFullScreen } from '../../hooks';
 import DialogButton from '../application/GenericDialog/DialogButton';
 import { useRouteState } from '../layout/store';
@@ -9,13 +9,17 @@ import { getDayTimeFromTimestamp, publicUrl } from '../../helpers';
 import { getAppName, getAppCompany, getAppIcon } from '../application/GenericTable/Applications/selectors';
 import ExpandableDescription from '../application/GenericTable/ApplicationsSummary/ExpandableDescription';
 
-const imageHeight = 128;
+const imageHeight = 144;
 
 const useStyles = makeStyles(({ palette }: any) =>
   createStyles({
     primaryLightText: {
       fontWeight: 700,
       color: palette.primary.light
+    },
+    bold: {
+      fontWeight: 900,
+      color: palette.common.black
     }
   })
 );
@@ -25,7 +29,7 @@ export default function ViewApp() {
   var sm = useFullScreen('sm');
 
   const [state] = useRouteState();
-  const { platforms, androidLink, iosLink, webLink, appleStore, androidStore, costs = [], updated, created } = state;
+  const { privacies, platforms, androidLink, iosLink, webLink, appleStore, androidStore, costs = [], updated, created } = state;
   const name = getAppName(state);
   const company = getAppCompany(state);
   const icon = getAppIcon(state);
@@ -33,49 +37,69 @@ export default function ViewApp() {
   const handleChangeRoute = useHandleChangeRoute();
 
   return (
-    <Grid container justify='center' style={{ padding: sm ? 16 : 32 }} spacing={sm ? 2 : 4}>
+    <Grid container justify='center' style={{ padding: sm ? 16 : 32 }} spacing={2}>
       <Grid item xs={12} style={{ cursor: 'pointer' }} onClick={handleChangeRoute(publicUrl('/Apps'), {})}>
         <Typography>{`<   Back To Results`}</Typography>
       </Grid>
       <Grid item xs={12}>
         <Grid container spacing={4}>
-          <Grid item xs style={{ minWidth: 300 }}>
-            <Grid container spacing={1}>
-              <Grid item style={{ maxWidth: imageHeight + 16 }}>
-                <img style={{ height: imageHeight }} src={icon} alt='logo' />
-              </Grid>
+          <Grid item style={{ width: imageHeight + 16 }}>
+            <img style={{ height: imageHeight }} src={icon} alt='logo' />
+          </Grid>
+          <Grid item xs>
+            <Grid container spacing={4}>
               <Grid item zeroMinWidth xs>
-                <Grid container spacing={1}>
+                <Grid container style={{ minWidth: 300 }}>
                   <Grid item xs={12}>
                     <Typography className={classes.primaryLightText} variant='h5'>
                       {name || 'Unknown Name'}
                     </Typography>
                     <Typography color='textSecondary'>{company}</Typography>
                   </Grid>
-                  <Grid item xs={12}>
+                  <Grid item xs={12} style={{ marginTop: 8, marginBottom: 8 }}>
                     <PlatformButtons platforms={platforms} androidLink={androidLink} iosLink={iosLink} webLink={webLink} />
                   </Grid>
                   <Grid item xs={12}>
-                    <Typography noWrap color='textSecondary' variant='caption'>
-                      {costs.length === 0 ? (
-                        'Unknown Cost'
-                      ) : costs.length > 2 ? (
-                        <DialogButton variant='link' size='small' Icon={null} tooltip={costs.join(' | ')}>
-                          Multiple Associated Costs
-                        </DialogButton>
-                      ) : (
-                        costs.join(' | ')
-                      )}
-                    </Typography>
+                    <Grid container spacing={1}>
+                      <Grid item>
+                        <Typography color='textSecondary' variant='caption'>
+                          Costs:
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography noWrap className={classes.primaryLightText} variant='caption'>
+                          {costs.length === 0 ? (
+                            'Unknown Cost'
+                          ) : costs.length > 2 ? (
+                            <DialogButton variant='link' size='small' Icon={null} tooltip={costs.join(' | ')}>
+                              Multiple Associated Costs
+                            </DialogButton>
+                          ) : (
+                            costs.join(' | ')
+                          )}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid container spacing={1}>
+                      <Grid item>
+                        <Typography color='textSecondary' variant='caption'>
+                          App Has Privacy Policy:
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography className={classes.primaryLightText} variant='caption'>
+                          {privacies.includes('Has Privacy Policy') ? 'Yes' : 'No'}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <ExpandableDescription appleStore={appleStore} androidStore={androidStore} />
-              </Grid>
             </Grid>
           </Grid>
-          <Grid item style={{ width: 240 }}>
+          <Grid item style={{ width: 248 }}>
             <Grid container spacing={1}>
               <Grid item xs={12}>
                 <DialogButton variant='primaryButton2' size='large' onClick={handleChangeRoute(publicUrl('/RateNewApp'), state)}>
@@ -86,16 +110,58 @@ export default function ViewApp() {
                 <DialogButton variant='arrowButton' label='Flag/Suggest an Edit' />
               </Grid>
               <Grid item xs={12}>
-                <Typography noWrap color='textSecondary' variant='caption'>
-                  Last App Rating: {updated ? getDayTimeFromTimestamp(updated) : created ? getDayTimeFromTimestamp(created) : ''}
-                </Typography>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <Grid container spacing={1}>
+                      <Grid item>
+                        <Typography color='textSecondary' variant='caption'>
+                          Last App Rating:
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography noWrap className={classes.primaryLightText} variant='caption'>
+                          {updated ? getDayTimeFromTimestamp(updated) : created ? getDayTimeFromTimestamp(created) : ''}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Grid container spacing={1}>
+                      <Grid item>
+                        <Typography color='textSecondary' variant='caption'>
+                          App Has Supported Studies:
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography className={classes.primaryLightText} variant='caption'>
+                          {privacies.includes('Has Privacy Policy') ? 'Yes' : 'No'}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
       <Grid item xs={12}>
-        More to be completed...
+        <Divider />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant='caption' className={classes.bold}>
+              Description from App Store, Not Vetted by MIND:
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <ExpandableDescription appleStore={appleStore} androidStore={androidStore} />
+          </Grid>
+          <Grid item xs={12}>
+            More to be completed...
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
