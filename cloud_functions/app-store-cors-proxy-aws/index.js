f41360ebe65848597b3875770ad367bdd9605e0d
Chris Van Emmerik <chris@greenlinkservices.com>
2022-02-09 23:35:09 -0700

    Filter un-used app store data from lambda function

--- cloud_functions/app-store-cors-proxy-aws/index.js
+++ cloud_functions/app-store-cors-proxy-aws/index.js
@@ -1,6 +1,16 @@
 var gplay = require('google-play-scraper');
 var apple = require('app-store-scraper');
 
+const androidKeys = ['appId', 'title', 'icon', 'developer', 'description', 'installs', 'offersIAP', 'free', 'adSupported', 'url', 'screenshots'];
+const appleKeys = ['appId', 'title', 'icon', 'developer', 'description', 'free', 'url', 'screenshots'];
+
+const filterKeys = (data, type) => {
+    const keys = type === 'apple' ? appleKeys : androidKeys;
+    var filteredData = {};
+    keys.forEach(k => (filteredData[k] = data[k]));
+    return filteredData;
+  };
+
 exports.handler = (event, context, callback) => {
   var appId = event['queryStringParameters'] && event['queryStringParameters']['appId'];
   var type = event['queryStringParameters'] && event['queryStringParameters']['type'];
@@ -23,7 +33,7 @@ exports.handler = (event, context, callback) => {
       success => {
         console.log({ Success: appId });
         response.statusCode = 200;
-        response.body = JSON.stringify(success);
+        response.body = JSON.stringify(filterKeys(success, type));
         callback(undefined, response);
       },
       error => {
