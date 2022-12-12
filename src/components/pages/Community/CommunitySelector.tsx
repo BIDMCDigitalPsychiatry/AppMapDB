efface92c05e4d184d2bbee68e47f738790270e7
Chris Van Emmerik <chris@greenlinkservices.com>
2022-12-12 10:53:28 -0700

    [Community] Remove calandar section

--- src/components/pages/Community/CommunitySelector.tsx
+++ src/components/pages/Community/CommunitySelector.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
-import { useHandleLink } from '../../../hooks';
 import TabSelectorTextToolBarNew from '../../general/TabSelector/TabSelectorTextToolBarNew';
 
 export const id = 'CommunitySelector';
 const CommunitySelector = ({ subRoute, ...other }) => {
-  const handleCalendarLink = useHandleLink('https://www.sodpsych.org/events');
+  //const handleCalendarLink = useHandleLink('https://www.sodpsych.org/events');
 
   const tabs = [
     { id: 'News', route: '/Community', routeState: { subRoute: 'list', cateogory: 'News' } },
     { id: 'Forum', route: '/Community', routeState: { subRoute: 'list', category: 'Forum' } },
-    { id: 'Calendar', route: '/Community', routeState: { subRoute: 'calendar' }, onClick: handleCalendarLink },
+    //{ id: 'Calendar', route: '/Community', routeState: { subRoute: 'calendar' }, onClick: handleCalendarLink },
     { id: 'Team', route: '/Community', routeState: { subRoute: 'team' }, altRoutes: ['viewTeamMember', 'createTeamMember', 'editTeamMember', 'viewTeamMember'] }
   ].filter(t => t);
 
