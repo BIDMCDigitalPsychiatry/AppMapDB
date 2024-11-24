6af1d635122b1b13dbe0cf9d045dbefd2e116328
Chris Van Emmerik <chris@greenlinkservices.com>
2024-11-24 16:11:52 -0700

    [PwaApps] Fix table height.

--- src/components/pwa/PwaApps.tsx
+++ src/components/pwa/PwaApps.tsx
@@ -2,16 +2,14 @@ import * as Tables from '../application/GenericTable';
 import { useTheme } from '@mui/material';
 import useAppTableData from '../pages/useAppTableData';
 import useHeight from '../layout/ViewPort/hooks/useHeight';
-import { useAppBarHeight, useHeaderHeight } from '../layout/hooks';
+import { useHeaderHeight } from '../layout/hooks';
 
 export default function PwaApps() {
   const [headerHeight] = useHeaderHeight();
-  const [appBarHeight] = useAppBarHeight();
   const height = useHeight();
   const { layout } = useTheme() as any;
   const { tablefooterheight } = layout;
-  //const tableHeight = height - headerHeight + tablefooterheight + 2 - 40;
-  const tableHeight = height - appBarHeight + tablefooterheight - headerHeight + 32;
+  const tableHeight = height + tablefooterheight - headerHeight + 8;
   const { filtered } = useAppTableData({ trigger: false }); // Don't re-trigger here as it causes a refresh
 
   return <Tables.PwaApplicationsGrid data={filtered} height={tableHeight} />;
