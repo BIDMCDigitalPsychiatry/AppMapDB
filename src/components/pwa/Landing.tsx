2e3d32c5a1debc9292e19c7cedfc5f47759dd019
Chris Van Emmerik <chris@greenlinkservices.com>
2024-11-26 12:10:11 -0700

    [PWA] Update landing logo and app bar logo.

--- src/components/pwa/Landing.tsx
+++ src/components/pwa/Landing.tsx
@@ -1,5 +1,5 @@
 import { Button, Box, Divider, Stack, Container, Grid } from '@mui/material';
-import logo from '../../images/logo_blue.png';
+import logo from '../../images/Brain.svg';
 import { usePwaActions } from './store';
 import useHeight from '../layout/ViewPort/hooks/useHeight';
 import useWidth from '../layout/ViewPort/hooks/useWidth';
@@ -32,9 +32,8 @@ const HorizontalLayout = () => {
   return (
     <Stack spacing={0} alignItems='center' justifyContent='space-evenly' height='100vh' px={2}>
       <Grid container justifyContent='space-around' alignItems='center' spacing={8}>
-        <Grid item>
-          <Header />
-          <img src={logo} alt='mindapp' style={{ width: 132, marginTop: 32 }} />
+        <Grid item sx={{ textAlign: 'center' }}>
+          <Header /> <img src={logo} alt='mindapp' style={{ width: 120, marginTop: 32 }} />
         </Grid>
         <Grid item xs>
           <Box sx={{ fontSize: 16, textAlign: 'center' }}>
@@ -56,7 +55,7 @@ const VerticalLayout = () => {
       <Box sx={{ fontSize: 16, textAlign: 'center', maxWidth: 400 }}>
         Answer a few questions, and we will recommend mental health apps that meet your needs and preferences.
       </Box>
-      <img src={logo} alt='mindapp' style={{ width: 160 }} />
+      <img src={logo} alt='mindapp' style={{ width: 140 }} />
       <Box sx={{ textAlign: 'center' }}>
         <StartButton />
       </Box>
--- src/components/pwa/PwaLogo.tsx
+++ src/components/pwa/PwaLogo.tsx
@@ -1,11 +1,11 @@
 import { Grid } from '@mui/material';
-import logo from '../../images/logoV2.png';
+import logo from '../../images/MIND_logo.svg';
 
 export default function PwaLogo() {
   return (
     <Grid container alignItems='center'>
       <Grid item>
-        <img src={logo} alt='logo' />
+        <img src={logo} alt='logo' style={{ maxHeight: 38 }} />
       </Grid>
     </Grid>
   );
