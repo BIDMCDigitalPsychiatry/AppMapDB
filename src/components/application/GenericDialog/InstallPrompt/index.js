527ced70568ac876c912b2a6f054496962784879
Chris Van Emmerik <chris@greenlinkservices.com>
2025-01-22 11:39:32 -0700

    Update install prompt dialog

--- src/components/application/GenericDialog/InstallPrompt/index.js
+++ src/components/application/GenericDialog/InstallPrompt/index.js
@@ -1,5 +1,5 @@
 import GenericDialog from '../GenericDialog';
-import { Grid, Box, DialogContent } from '@mui/material';
+import { Box, DialogContent, Typography } from '@mui/material';
 import * as Icons from '@mui/icons-material';
 
 export const title = 'Install To Home Screen';
@@ -8,20 +8,26 @@ export default function InstallPromptDialog({ id = title }) {
   return (
     <GenericDialog id={id} title={id} submitLabel={null} cancelLabel='Close' maxWidth='sm'>
       <DialogContent dividers>
-        <Box sx={{ fontSize: 24, fontWeight: 700, color: 'text.primary', textAlign: 'center' }}>Add MINDapps to your home screen!</Box>
-        <Grid container spacing={1} alignItems='center' sx={{ mt: 1 }}>
-          <Grid item>
-            <Box sx={{ fontSize: 15, pt: 2, color: 'text.primary', textAlign: 'left', display: 'inline' }}>{`1. Press the share symbol`}</Box>
-          </Grid>
-          <Grid item>
-            <Icons.IosShare color='text.secondary' />
-          </Grid>
-          <Grid item>
-            <Box sx={{ fontSize: 15, pt: 2, color: 'text.primary', textAlign: 'left', display: 'inline' }}>{`at the bottom (or top) of your screen.`}</Box>
-          </Grid>
-        </Grid>
-        <Box sx={{ fontSize: 15, pt: 2, color: 'text.primary', textAlign: 'left' }}>2. Select “Add to Home Screen”</Box>
-        <Box sx={{ fontSize: 15, pt: 2, color: 'text.primary', textAlign: 'left' }}>3. Press “Add” in the top right corner</Box>
+        <Box sx={{ fontSize: 24, fontWeight: 700, color: 'text.primary', textAlign: 'center', mb: 4 }}>Add MINDapps to your home screen!</Box>
+        <Box sx={{ display: 'flex', mt: 2 }}>
+          <Typography
+            sx={{ fontSize: 15, color: 'text.primary', textAlign: 'left', display: 'inline' }}
+          >{`1. Press the share symbol at the bottom or top of your screen.`}</Typography>
+          <Box sx={{ pt: 0, ml: 0.5, display: 'inline' }}>
+            <Icons.IosShare color='text.secondary' sx={{ mx: 0.5 }} />
+          </Box>
+        </Box>
+
+        <Box sx={{ display: 'flex', mt: 2 }}>
+          <Typography
+            sx={{ fontSize: 15, color: 'text.primary', textAlign: 'left', display: 'inline' }}
+          >{`2. Select the "Add to Home Screen" option.`}</Typography>
+          <Box sx={{ pt: 0, ml: 0.5, display: 'inline' }}>
+            <Icons.AddBoxOutlined color='text.secondary' />
+          </Box>
+        </Box>
+
+        <Box sx={{ fontSize: 15, mt: 2, mb: 2, color: 'text.primary', textAlign: 'left' }}>3. Press “Add” in the top right corner</Box>
       </DialogContent>
     </GenericDialog>
   );
