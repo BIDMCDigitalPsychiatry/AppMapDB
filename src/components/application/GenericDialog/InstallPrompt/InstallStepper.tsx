fedfda17dc750ed78ec3f122c45c87db658ff762
Chris Van Emmerik <chris@greenlinkservices.com>
2025-01-30 20:18:12 -0700

    [InstallStepper] Code cleanup

--- src/components/application/GenericDialog/InstallPrompt/InstallStepper.tsx
+++ src/components/application/GenericDialog/InstallPrompt/InstallStepper.tsx
@@ -3,9 +3,6 @@ import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import StepContent from '@mui/material/StepContent';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import * as Icons from '@mui/icons-material';
 
