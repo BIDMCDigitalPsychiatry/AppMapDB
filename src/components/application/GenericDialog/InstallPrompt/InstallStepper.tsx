72182fc6094d140f368fcb96613b0ccd900de5d4
Chris Van Emmerik <chris@greenlinkservices.com>
2025-01-30 20:07:00 -0700

    [InstallPrompt] Add InstallStepper.

---
+++ src/components/application/GenericDialog/InstallPrompt/InstallStepper.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import * as Icons from '@mui/icons-material';
+
+const fontSize = 18;
+const StepIcon = props => <Icons.Circle {...props} sx={{ color: 'primary.dark' }} />;
+
+export default function InstallStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{}}>
+      <Stepper orientation='vertical'>
+        <Step key={0} active={true}>
+          <StepLabel StepIconComponent={StepIcon}>
+            <Box sx={{ display: 'flex', ml: 2 }}>
+              <Typography
+                sx={{ fontSize, color: 'text.primary', textAlign: 'left', display: 'inline' }}
+              >{`Press the share symbol at the bottom or top of your screen.`}</Typography>
+              <Box sx={{ pt: 0, ml: 1, display: 'inline' }}>
+                <Icons.IosShare sx={{ fontSize: fontSize * 2 }} />
+              </Box>
+            </Box>
+          </StepLabel>
+        </Step>
+        <Step key={1} active={true}>
+          <StepLabel StepIconComponent={StepIcon}>
+            <Box sx={{ display: 'flex', ml: 2 }}>
+              <Typography
+                sx={{ fontSize, color: 'text.primary', textAlign: 'left', display: 'inline' }}
+              >{`Select the "Add to Home Screen" option.`}</Typography>
+              <Box sx={{ pt: 0, ml: 2, display: 'inline' }}>
+                <Icons.AddBoxOutlined sx={{ fontSize: fontSize * 2 }} />
+              </Box>
+            </Box>
+          </StepLabel>
+        </Step>
+        <Step key={2} active={true}>
+          <StepLabel StepIconComponent={StepIcon}>
+            <Box sx={{ fontSize, color: 'text.primary', textAlign: 'left', ml: 2 }}>Press “Add” in the top right corner</Box>
+          </StepLabel>
+        </Step>
+      </Stepper>
+    </Box>
+  );
+}
--- src/components/application/GenericDialog/InstallPrompt/index.js
+++ src/components/application/GenericDialog/InstallPrompt/index.js
@@ -4,6 +4,7 @@ import { Box, Button, DialogContent, Typography } from '@mui/material';
 import * as Icons from '@mui/icons-material';
 import { useDialogState } from '../useDialogState';
 import { buttonMinHeight, buttonWidth } from '../../../pwa/Landing';
+import InstallStepper from './InstallStepper';
 
 export const title = 'Install To Home Screen';
 
@@ -40,22 +41,7 @@ export default function InstallPromptDialog({ id = title }) {
     <GenericDialog id={id} title={id} submitLabel={null} cancelLabel='Close' maxWidth='sm' titleBackgroundColor='primary.dark'>
       <DialogContent dividers>
         <Box sx={{ fontSize: 24, fontWeight: 700, color: 'text.primary', mb: 4 }}>Add MINDapps to your home screen!</Box>
-        <Box sx={{ display: 'flex', mt: 2 }}>
-          <Typography
-            sx={{ fontSize, color: 'text.primary', textAlign: 'left', display: 'inline' }}
-          >{`1. Press the share symbol at the bottom or top of your screen.`}</Typography>
-          <Box sx={{ pt: 0, ml: 0.5, display: 'inline' }}>
-            <Icons.IosShare color='text.secondary' sx={{ mx: 0.5 }} />
-          </Box>
-        </Box>
-
-        <Box sx={{ display: 'flex', mt: 2 }}>
-          <Typography sx={{ fontSize, color: 'text.primary', textAlign: 'left', display: 'inline' }}>{`2. Select the "Add to Home Screen" option.`}</Typography>
-          <Box sx={{ pt: 0, ml: 0.5, display: 'inline' }}>
-            <Icons.AddBoxOutlined color='text.secondary' />
-          </Box>
-        </Box>
-        <Box sx={{ fontSize, mt: 2, color: 'text.primary', textAlign: 'left' }}>3. Press “Add” in the top right corner</Box>
+        <InstallStepper />
         <Box sx={{ fontSize, mt: 4, mb: 3, color: 'text.primary', textAlign: 'center' }}>
           <CloseButton onClick={handleClose} />
         </Box>
